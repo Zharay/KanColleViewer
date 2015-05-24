7708a589328ac24d1c95ea5f6b8d1da7d7a9a11b
Yuubari <remilia@koumakan.jp>
2015-05-24 15:53:47 +0300

    Attempt to re-implement item stats display

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemCounter.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemCounter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleViewer.Properties;
 
 namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 {
@@ -37,6 +38,36 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 		public void AddShip(Ship ship, int itemLevel)
 		{
 			this.itemsByLevel[itemLevel].AddShip(ship);
+		}
+
+		public string Detail
+		{
+			get
+			{
+				string AddDetail = "";
+
+				if (this.Target.Firepower > 0) AddDetail += " +" + this.Target.Firepower + " " + Resources.Stats_Firepower;
+				if (this.Target.AA > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.AA + " " + Resources.Stats_AntiAir;
+				if (this.Target.Torpedo > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.Torpedo + " " + Resources.Stats_Torpedo;
+				if (this.Target.AntiSub > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.AntiSub + " " + Resources.Stats_AntiSub;
+				if (this.Target.SightRange > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.SightRange + " " + Resources.Stats_SightRange;
+				if (this.Target.Speed > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.Speed + " " + Resources.Stats_Speed;
+				if (this.Target.Armor > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.Armor + " " + Resources.Stats_Armor;
+				if (this.Target.Health > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.Health + " " + Resources.Stats_Health;
+				if (this.Target.Luck > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.Luck + " " + Resources.Stats_Luck;
+				if (this.Target.Evasion > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.Evasion + " " + Resources.Stats_Evasion;
+				if (this.Target.Accuracy > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.Accuracy + " " + Resources.Stats_Accuracy;
+				if (this.Target.DiveBomb > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.DiveBomb + " " + Resources.Stats_DiveBomb;
+				if (this.Target.AttackRange > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " " + Resources.Stats_AttackRange + " (" + this.Target.AttackRange + ")";
+				//if (this.Target.RawData.api_raik > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.RawData.api_raik + " api_raik";
+				//if (this.Target.RawData.api_raim > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.RawData.api_raim + " api_raim";
+				//if (this.Target.RawData.api_sakb > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.RawData.api_sakb + " api_sakb";
+				//if (this.Target.RawData.api_atap > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.RawData.api_atap + " api_atap";
+				//if (this.Target.RawData.api_rare > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.RawData.api_rare + " api_rare";
+				//if (this.Target.RawData.api_bakk > 0) AddDetail += (AddDetail != "" ? "\n" : "") + " +" + this.Target.RawData.api_bakk + " api_bakk";
+
+				return AddDetail;
+			}
 		}
 	}
 
